548e5c62cfaaa5adf2c65592bc0ec49e6069fc28
David Adler <dalberto.adler@gmail.com>
2014-01-25 02:48:46 -0200

    sorted out view stuff

--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -6,17 +6,3 @@ Ember.Handlebars.helper('formatMarkdown', function(text) {
     html = window.converter.makeHtml(text);
     return new Handlebars.SafeString(html);
 });
-Ember.Handlebars.helper('aceEditor', function() {
-    setTimeout(function() {
-        var editor = ace.edit("editor");
-        editor.setTheme("ace/theme/monokai");
-        var MarkdownMode = require("ace/mode/markdown").Mode;
-        editor.getSession().setMode(new MarkdownMode());
-        var textarea = $('textarea[name="editor"]').hide();
-        editor.getSession().setValue(textarea.val());
-        editor.getSession().setUseWrapMode(true);
-        editor.getSession().on('change', function() {
-            textarea.val(editor.getSession().getValue()).trigger('change');
-        });
-    }, 1);
-});
